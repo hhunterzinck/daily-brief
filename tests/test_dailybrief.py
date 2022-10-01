9ecfce6b6f18693585f63d177fb5c43304adde86
hhunterzinck <haleyhunterzinck@gmail.com>
2022-10-01 15:46:38 -0400

    add tests for message component functions

--- tests/test_dailybrief.py
+++ tests/test_dailybrief.py
@@ -11,8 +11,21 @@ from dailybrief.dailybrief import DailyBrief
 
 
 briefer = DailyBrief()
+runs = ['a','b','c','d','e']
 
 def test_set_seed_by_date():
     seed_yesterday = briefer.set_seed_by_date(date(2022, 9, 30))
     seed_today = briefer.set_seed_by_date(date(2022, 10, 1))
     assert seed_yesterday != seed_today
+
+def test_get_run_one():
+    run = briefer.get_run(runs=runs)
+    assert len(run) == 1
+
+def test_get_run_sample():
+    run = briefer.get_run(runs=runs)
+    assert run in runs
+    
+def test_get_countdown_zero():
+    countdown = briefer.get_countdown(format(date.today(), '%Y-%m-%d'))
+    assert countdown == 0
